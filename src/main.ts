591755b482100e136968f3a187cd4619131887e2
Кальченко Тимофей <hackerbro228007@mail.ru>
2024-10-05 11:10:53 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import { VueTelegramPlugin } from 'vue-tg'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueTelegramPlugin)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { Alert } from 'vue-tg'
+
+function AlertFunc(){
+  return "<Alert message='Кирилл Баран'/>"
+}
+</script>
+
+<template>
+  <main>
+<!--    Main Page-->
+    <Alert message="Kirill Baran"/>
+    Score: {{score}}<br>
+    PassiveScore When You Online: {{passiveScore}}<br>
+    ScoreUp: {{scoreUp}}<br>
+    {{scoreUpdate}}<br><br>
+    <button @click="mainBtn">Click to UP YOUR SCORE!</button><br><br>
+    <!--    <Alert message="Hello TgMiniApp!!"/>-->
+  </main>
+</template>
+<script lang="ts">
+
+export default {
+  components: {
+    Alert
+  },
+  data() {
+    return {
+      score: 0,
+      scoreUp: 1,
+      passiveScore: 10,
+    }
+  },
+  methods: {
+    mainBtn: function () {
+      this.score+= this.scoreUp
+    }
+  },
+  computed: {
+    scoreUpdate: function () {
+      setInterval((id: 2) => {
+        this.score += this.passiveScore
+        clearInterval(id)
+      }, 2000)
+    }
+  }
+}
+</script>

117e3d4a457c486ea7c46edd3dc33a410edf22e5
Кальченко Тимофей <hackerbro228007@mail.ru>
2024-10-07 11:42:11 +0300

    trying to get an userID to insert to DB

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,8 @@ let UserDataSTR = JSON.stringify(decodeURIComponent(user)).replace('user=', '');
 let UserToJson = UserDataSTR.replace(/}/, '');
 UserToJson = UserToJson.replace(/&/g, ',');
 UserToJson = UserToJson.replace(/=/g, ':');
-// UserToJson = UserToJson + "}";
+UserToJson = UserToJson.slice(0, -1);
+UserToJson += "}";
 // UserToJson = JSON.parse(UserToJson);
 console.log(UserToJson);
 
